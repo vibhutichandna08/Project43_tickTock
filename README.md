222b7deda43471feb8c8320d3adf928a20861154
Vibhuti Chandna <75322300+vibhutichandna08@users.noreply.github.com>
2021-02-08 21:18:08 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Project 43
-Project 43
+# p5.play-boilerplate
+Boiler plate for p5.play
--- sketch.js
+++ sketch.js
@@ -1,57 +1,66 @@
-var hr, min , sec;
-var hrAngle, minAngle, secAngle;
+var hr, mn, sc;
+var hrAngle, mnAngle, scAngle; 
 
 function setup(){
-    createCanvas(400, 400);
-
-    angleMode(DEGREES);
+    createCanvas(400,400); 
+    //To use angle in Degrees
+    angleMode(DEGREES);   
 }
 
 function draw(){
     background(0);
 
-    translate(200, 200);
-    rotate(-90);
+    //Translation and rotation 
+    translate(200,200)
+    rotate(-90)
 
+    //Calculating time using predefined func from p5.js
     hr = hour();
-    min = minute();
-    sec = second();
+    mn = minute();
+    sc = second();
 
-    secAngle = map(sec, 0, 60, 0, 360);
+    //To create ab iterative rotation
+    scAngle = map(sc, 0, 60, 0, 360);
+    mnAngle = map(mn,0,60,0,360)
+    hrAngle = map(hr % 12,0,12,0,360)
+
+    //drawing seconds hand
     push();
-    rotate(secAngle);
-    stroke(255, 0, 0);
+    rotate(scAngle); //rotate the hand based on angle calculated
+    stroke(255,0,0);
     strokeWeight(7);
-    line(0, 0, 100, 0);
-    pop();
+    line(0,0,100,0);
+    pop()
 
-    minAngle = map(min, 0, 60, 0, 360);
+    //drawing mins hand
     push();
-    rotate(minAngle);
-    stroke(0, 255, 0);
+    rotate(mnAngle);
+    stroke(0,255,0);
     strokeWeight(7);
-    line(0, 0, 75, 0);
-    pop();
+    line(0,0,75,0);
+    pop()
 
-    hrAngle = map(hr, 0, 60, 0, 360);
+    //drawing hr hand
     push();
     rotate(hrAngle);
-    stroke(0, 0, 255);
+    stroke(0,0,255);
     strokeWeight(7);
-    line(0, 0, 50, 0);
-    pop();
+    line(0,0,50,0);
+    pop()
 
+    stroke(255,0,255);
+    point(0,0)
+
+    //drawing the arcs
     strokeWeight(10);
     noFill();
-    //hours
-    stroke(0, 0, 255);
-    arc(0, 0, 260, 260, 0, hrAngle);
-
-    //minutes
-    stroke(0, 255, 0);
-    arc(0, 0, 280, 280, 0, minAngle);
-
-    //seconds
-    stroke(255, 0, 0);
-    arc(0, 0,300, 300, 0, secAngle);
+    //Seconds
+    stroke(255,0,0);
+    arc(0,0,300,300,0,scAngle);
+    //Minutes
+    stroke(0,255,0);
+    arc(0,0,280,280,0,mnAngle);
+    //Hour
+    stroke(0,0,255);
+    arc(0,0,260,260,0,hrAngle);
 }
